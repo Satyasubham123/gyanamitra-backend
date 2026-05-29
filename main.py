2f72d7fe0453c0fdf570a005ca2f4ca0365184d4
Satyasubham123 <biswalsatyasubham2006@gmail.com>
2026-05-29 07:36:27 +0530

    Fix CORS nested list typo

--- main.py
+++ main.py
@@ -162,7 +162,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[origins], 
+    allow_origins=origins, 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
